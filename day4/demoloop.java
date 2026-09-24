0e89cfb627d26cecb4460039b7b8db3c45c644c2
hemanthnali8296-cell <hemanthnali8296@gmail.com>
2026-09-24 19:51:08 +0530

    Add files via upload

---
+++ day4/demoloop.java
@@ -0,0 +1,25 @@
+package day3;
+
+import java.util.Scanner;
+
+public class demoloop {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        int limit=0,number=0,prev1=Integer.MIN_VALUE;
+        int  prev2 = Integer.MIN_VALUE;
+        System.out.println("Let us Know the Limit");
+        limit=scan.nextInt();
+        while(limit>0) {
+            System.out.println("Enter the number");
+            number = scan.nextInt();
+            if(prev1<number){prev2=prev1;prev1=number;}
+            else if (prev2<number&&number!=prev1) {
+                prev2=number;
+                limit--;
+            }
+          
+        }
+          System.out.println(prev1+" "+prev2);
+           scan.close(); 
+    }
+}
